f3bf9c5d38de742112978193d4470ad107de396e
MiodragKruscic <miokruscic@gmail.com>
2020-04-16 23:07:02 +0200

    made slider responsive

--- js/slider.js
+++ js/slider.js
@@ -2,25 +2,31 @@ const slider = document.querySelector(".slider-container");
 const sliderCont = document.querySelectorAll(".slider-content");
 const leftB = document.querySelector("#leftArrow");
 const rightB = document.querySelector("#rightArrow");
-
 let sliderContentCounter = 0; //regulates the movement of the slider content
 
-rightB.addEventListener("click", ()=>{
-    const moveBySize = sliderCont[5].clientWidth + 10; //this variable is defined every time the function is called. This way, the slider content moves for its whole length, regardless of the viewport width
-    slider.style.transition= "transform 0.4s linear"
-    sliderContentCounter += 1;
+leftB.addEventListener("click",foreward);
+rightB.addEventListener("click",foreward);
+
+
+function foreward(e) {
+    let moveBySize = sliderCont[5].clientWidth + (sliderCont[5].clientWidth/100) + (sliderCont[5].clientWidth/100) + (sliderCont[5].clientWidth/114);//this variable is defined every time the function is called. This way, the slider content moves for its whole length, regardless of the viewport width
+        if(screen.width <= 650){
+            moveBySize = sliderCont[5].clientWidth;
+        }
+    slider.style.transition= "transform 0.4s linear";
+
+    if(e.target === leftB){
+        sliderContentCounter += -1;
+    }
+    else{
+        sliderContentCounter += 1;
+    }
+
+
     if(sliderContentCounter > sliderCont.length-3){
         sliderContentCounter = 0;
     }
     slider.style.transform= "translateX(" + (-moveBySize * sliderContentCounter) + "px)"; //moves the content
-});
+}
 
-leftB.addEventListener("click", ()=>{
-    const moveBySize = sliderCont[5].clientWidth + 10;
-    slider.style.transition= "transform 0.4s linear"
-    sliderContentCounter += -1;
-    if(sliderContentCounter < 0){
-        sliderContentCounter = sliderCont.length-3;
-    }
-    slider.style.transform= "translateX(" + (-moveBySize * sliderContentCounter) + "px)";
-});
+setInterval(foreward,3000,2);
