c90e99cf93573220dfdfff78d8e14fb48aed507c
MiodragKruscic <miokruscic@gmail.com>
2020-04-17 16:34:23 +0200

    small changes to header

--- js/header.js
+++ js/header.js
@@ -1,10 +1,10 @@
 header = document.querySelector("header");
 
 window.addEventListener("scroll", function(e){
-    if(window.scrollY>0){
+    if(window.scrollY>10){
         header.style.boxShadow="0px 10px 11px -1px rgba(0,0,0,0.75)";
     }
     else {
-        header.style.boxShadow="none"
+        header.style.boxShadow="none";
     }
 })
