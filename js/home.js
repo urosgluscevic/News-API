9b1213e886e429049f2f5da6d8ad7bf00d7cf94c
LRistanovic <lukaristanovic3@gmail.com>
2020-04-18 12:33:40 +0200

    home - top rated (basics and layout)

--- js/home.js
+++ js/home.js
@@ -8,11 +8,5 @@ async function getNews() {
     let data = await response.json()
     console.log(data)
 }
-/*
-fetch("https://newsapi.org/v2/top-headlines?country=rs&apiKey=d6e9eca6a9e4407cb8c19ed7dd38b910")
-    .then(data => data.json())
-    .then(json => {
-        console.log(json)
-    })*/
 
 getNews()
