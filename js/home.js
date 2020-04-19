3e336179d18820342cf9e4d5ddb3d4cfa074ec77
LRistanovic <lukaristanovic3@gmail.com>
2020-04-19 20:07:58 +0200

    top rated finished and responsivity additions

--- js/home.js
+++ js/home.js
@@ -1,3 +1,103 @@
+//EVERYTHING BELOW THIS LINE IS USED FOR THE FUNCTIONALITY OF THE "LOAD MORE" BUTTON
+
+var button = document.getElementById("top-rated-more");
+var newsContainer = document.getElementById("top-rated");
+var newsContainerGrid = document.getElementById("top-rated-grid");
+var main = document.getElementById("main");
+
+
+let numOfAddedArticles = 0
+
+window.addEventListener("resize", correctWidth)
+
+function correctWidth() {
+    let mainBaseHeight,
+        topRatedBaseHeight,
+        topRatedGridBaseHeight,
+        numOfAddedRows = 0
+
+    if (window.innerWidth > 650) {
+        mainBaseHeight = 43.5
+        topRatedBaseHeight = 26.625
+        topRatedGridBaseHeight = 22.5
+        numOfAddedRows = Math.ceil(numOfAddedArticles / 3)
+
+        console.log("desktop")
+    } else {
+        mainBaseHeight = 85
+        topRatedBaseHeight = 38.125
+        topRatedGridBaseHeight = 34
+        numOfAddedRows = Math.ceil(numOfAddedArticles / 2)
+
+        console.log("mobile")
+    }
+
+    for (let i = 0; i < numOfAddedRows; i++) {
+        mainBaseHeight += 11.5
+        topRatedBaseHeight += 11.5
+        topRatedGridBaseHeight += 11.5
+    }
+    
+
+    main.style.height = mainBaseHeight + "rem"
+    newsContainer.style.height = topRatedBaseHeight + "rem"
+    newsContainerGrid.style.height = topRatedGridBaseHeight + "rem"
+}
+
+// const loadMoreHandler = higherOrder()
+button.addEventListener("click", loadMoreHandler)
+
+var currentMainHeightValue = main.offsetHeight / 16; //this is the current height of #main
+var currentGridContainerHeightValue = newsContainerGrid.offsetHeight / 16; //same as #main
+var currentNewsContainerHeightValue = newsContainer.offsetHeight / 16;
+var increaseHeightBy = 11.5; //every time the function is called, it will be incrased my this value (in rem)
+
+
+var buttonPressed = 0; // controls the number of articles that have been used
+
+function loadMoreHandler(){
+    
+    if(buttonPressed < Math.floor((data.articles.length - 6) / 3)) {
+        for(i = 0; i < 3; i++){
+            numOfAddedArticles++
+
+            var newBlock = document.createElement("div");
+            var paragraph1 = document.createElement("p");
+            var paragraph2 = document.createElement("p");
+            var imgContainer = document.createElement("div");
+            var imageSpot = document.createElement("img")
+
+            imgContainer.classList.add("top-rated-imgContainer");
+            newBlock.classList.add("top-rated-item");
+            imageSpot.classList.add("top-rated-img");
+
+            imgContainer.appendChild(imageSpot);
+
+            newBlock.appendChild(paragraph1);
+            newBlock.appendChild(paragraph2);
+            newBlock.appendChild(imgContainer);
+
+            newBlock.children[0].textContent = data.articles[alreadyUsed].title;
+            newBlock.children[1].textContent = data.articles[alreadyUsed].description;
+            newBlock.children[2].firstElementChild.src = data.articles[alreadyUsed].urlToImage;
+
+            newBlock.addEventListener("click",modalPopUp);
+            
+            newsContainerGrid.appendChild(newBlock)
+
+            alreadyUsed++;
+        }
+
+        correctWidth()
+
+        buttonPressed++;
+    }
+}
+
+var alreadyUsed = 0; //is used in order for new articles to be loaded, and for ignoring the old ones.
+
+// EVERYTHING ABOVE THIS LINE IS USED FOR THE FUNCTIONALITY OF THE "LOAD MORE" BUTTON
+
 async function fillNews(item, index) {
     const response = await fetch("https://newsapi.org/v2/top-headlines?country=rs", {
         headers: {
