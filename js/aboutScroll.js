b222b42ada8a3cb25a95eeecdfd9051e7b32cce2
MiodragKruscic <miokruscic@gmail.com>
2020-04-29 21:58:39 +0200

    final commit

--- js/aboutScroll.js
+++ js/aboutScroll.js
@@ -1,5 +1,5 @@
 document.addEventListener("click", function(e){
-    if(e.target.classList.contains("dropdown-link")){
+    if(e.target.classList.contains("dropdown-link") || e.target.innerText === "Uros" || e.target.innerText === "Luka" || e.target.innerText === "Miodrag"){
         sessionStorage.setItem("scrollTo", e.target.innerText)
     } else if(e.target.innerText === "ABOUT"){
         sessionStorage.setItem("scrollTo", "ABOUT")
